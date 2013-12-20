808fa7d82d502f5c00eae6ef9dcba9694ad77e11
yrmt <youri.mout@gmail.com>
2013-12-20 19:11:59 +0100

    much needed cleanup

--- clock.c
+++ clock.c
@@ -6,6 +6,7 @@
 #include <ncurses.h>
 #include <signal.h>
 #include <getopt.h>
+#include <stdbool.h>
 /* User changeable options. */
 /* Available:
  * COLOR_BLACK
@@ -20,282 +21,213 @@
 #define DEFAULT_FG_COLOR COLOR_BLUE
 #define DEFAULT_FG_COLOR_NAME "blue"
 
-typedef enum { False, True } Bool;
-
-/* Global cliclock struct */
 typedef struct
 {
- /* while() boolean */
- Bool running;
- int bg;
- struct
- {
-    int color;
-    long delay;
- } option;
+    bool running;
+    int bg;
+    struct
+    {
+        int color;
+        long delay;
+    } option;
 
- /* Clock geometry */
- struct
- {
-    int x, y, w, h;
- } geo;
+    struct
+    {
+        int x, y, w, h;
+    } geo;
 
- /* Date content ([2] = number by number) */
- struct
- {
-    unsigned int hour[2];
-    unsigned int minute[2];
-    unsigned int second[2];
- } date;
+    /* Date content ([2] = number by number) */
+    struct
+    {
+        unsigned int hour[2];
+        unsigned int minute[2];
+        unsigned int second[2];
+    } date;
 
- /* time.h utils */
- struct tm *tm;
- time_t lt;
+    struct tm *tm;
+    time_t lt;
 
- /* Clock member */
- WINDOW *framewin;
+    WINDOW *framewin;
 
 } cliclock_t;
 
-/* Prototypes */
-void init(void);
-void update_hour(void);
-void draw_number(int n, int x, int y);
-void draw_clock(void);
-void clock_move(int x, int y);
-void set_win(void);
-void signal_handler(int signal);
-void usage(void);
+static void init(void);
+static void update_hour(void);
+static void draw_number(int n, int x, int y);
+static void draw_clock(void);
+static void clock_move(int x, int y);
+static void signal_handler(int signal);
 
-/* Global variable */
 cliclock_t *cliclock;
 
-/* Number matrix */
-const Bool number[][15] =
-{
-  {1,1,1,1,0,1,1,0,1,1,0,1,1,1,1}, /* 0 */
-  {0,0,1,0,0,1,0,0,1,0,0,1,0,0,1}, /* 1 */
-  {1,1,1,0,0,1,1,1,1,1,0,0,1,1,1}, /* 2 */
-  {1,1,1,0,0,1,1,1,1,0,0,1,1,1,1}, /* 3 */
-  {1,0,1,1,0,1,1,1,1,0,0,1,0,0,1}, /* 4 */
-  {1,1,1,1,0,0,1,1,1,0,0,1,1,1,1}, /* 5 */
-  {1,1,1,1,0,0,1,1,1,1,0,1,1,1,1}, /* 6 */
-  {1,1,1,0,0,1,0,0,1,0,0,1,0,0,1}, /* 7 */
-  {1,1,1,1,0,1,1,1,1,1,0,1,1,1,1}, /* 8 */
-  {1,1,1,1,0,1,1,1,1,0,0,1,1,1,1}, /* 9 */
+static const bool number[][15] = {
+    {1,1,1,1,0,1,1,0,1,1,0,1,1,1,1}, /* 0 */
+    {0,0,1,0,0,1,0,0,1,0,0,1,0,0,1}, /* 1 */
+    {1,1,1,0,0,1,1,1,1,1,0,0,1,1,1}, /* 2 */
+    {1,1,1,0,0,1,1,1,1,0,0,1,1,1,1}, /* 3 */
+    {1,0,1,1,0,1,1,1,1,0,0,1,0,0,1}, /* 4 */
+    {1,1,1,1,0,0,1,1,1,0,0,1,1,1,1}, /* 5 */
+    {1,1,1,1,0,0,1,1,1,1,0,1,1,1,1}, /* 6 */
+    {1,1,1,0,0,1,0,0,1,0,0,1,0,0,1}, /* 7 */
+    {1,1,1,1,0,1,1,1,1,1,0,1,1,1,1}, /* 8 */
+    {1,1,1,1,0,1,1,1,1,0,0,1,1,1,1}, /* 9 */
 };
-
-void
-init(void)
+static void init(void)
 {
- struct sigaction sig;
- cliclock->bg = 0;
+    struct sigaction sig;
+    cliclock->bg = 0;
 
- /* Init ncurses */
- initscr();
- cbreak();
- noecho();
- keypad(stdscr, True);
- start_color();
- curs_set(False);
- clear();
- if(use_default_colors() == OK)
- cliclock->bg = -1;
- /* Init color pair */
- init_pair(0, cliclock->bg, cliclock->bg);
- init_pair(1, cliclock->bg, cliclock->option.color);
- init_pair(2, cliclock->option.color, cliclock->bg);
-		 
- refresh();
-    
- /* Init signal handler */
- sig.sa_handler = signal_handler;
- sig.sa_flags   = 0;
- sigaction(SIGWINCH, &sig, NULL);
- sigaction(SIGTERM,  &sig, NULL);
+    initscr();
+    cbreak();
+    noecho();
+    keypad(stdscr, true);
+    start_color();
+    curs_set(false);
+    clear();
+    if(use_default_colors() == OK)
+        cliclock->bg = -1;
+    /* Init color pair */
+    init_pair(0, cliclock->bg, cliclock->bg);
+    init_pair(1, cliclock->bg, cliclock->option.color);
+    init_pair(2, cliclock->option.color, cliclock->bg);
 
- /* Init global struct */ 
- cliclock->running = True;
- if(!cliclock->geo.x)
- cliclock->geo.x = 0;
- if(!cliclock->geo.y)
- cliclock->geo.y = 0;
- cliclock->geo.w = 54;
- cliclock->geo.h = 7;
- cliclock->tm = localtime(&(cliclock->lt));
- cliclock->lt = time(NULL);
- update_hour();
+    refresh();
 
- /* Create clock win */ 
- cliclock->framewin = newwin(cliclock->geo.h,
-			     cliclock->geo.w,
-		      	     cliclock->geo.x,
-		       	     cliclock->geo.y);
- clock_move((LINES / 2 - (cliclock->geo.h / 2)),
-	   (COLS  / 2 - (cliclock->geo.w / 2)));
- wborder(cliclock->framewin, ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ');
- nodelay(stdscr, True);
- wrefresh(cliclock->framewin);
+    /* Init signal handler */
+    sig.sa_handler = signal_handler;
+    sig.sa_flags   = 0;
+    sigaction(SIGWINCH, &sig, NULL);
+    sigaction(SIGTERM,  &sig, NULL);
 
- return;
+    /* Init global struct */ 
+    cliclock->running = true;
+    if(!cliclock->geo.x)
+        cliclock->geo.x = 0;
+    if(!cliclock->geo.y)
+        cliclock->geo.y = 0;
+    cliclock->geo.w = 54;
+    cliclock->geo.h = 7;
+    cliclock->tm = localtime(&(cliclock->lt));
+    cliclock->lt = time(NULL);
+    update_hour();
+
+    /* Create clock win */ 
+    cliclock->framewin = newwin(cliclock->geo.h,
+            cliclock->geo.w,
+            cliclock->geo.x,
+            cliclock->geo.y);
+    clock_move((LINES / 2 - (cliclock->geo.h / 2)),
+            (COLS  / 2 - (cliclock->geo.w / 2)));
+    wborder(cliclock->framewin, ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ');
+    nodelay(stdscr, true);
+    wrefresh(cliclock->framewin);
+
+    return;
 }
-void
-signal_handler(int signal)
+static void signal_handler(int signal)
 {
-switch(signal)
+    switch(signal)
+    {
+        case SIGWINCH: /* window resize signal */
+            endwin();
+            init();
+            break;
+        case SIGTERM:
+            cliclock->running = false; /* interruption signal */
+    }
+    return;
+}
+static void update_hour(void)
 {
-   case SIGWINCH: /* window resize signal */
-	endwin();
-	init();
-	break;
-   case SIGTERM:
-	cliclock->running = False; /* interruption signal */
+    int ihour;
+    cliclock->tm = localtime(&(cliclock->lt));
+    cliclock->lt = time(NULL);
+    ihour = cliclock->tm->tm_hour;
+    /* Set hour */
+    cliclock->date.hour[0] = ihour / 10;
+    cliclock->date.hour[1] = ihour % 10;
+    /* Set minutes */
+    cliclock->date.minute[0] = cliclock->tm->tm_min / 10;
+    cliclock->date.minute[1] = cliclock->tm->tm_min % 10;
+    /* Set seconds */
+    cliclock->date.second[0] = cliclock->tm->tm_sec / 10;
+    cliclock->date.second[1] = cliclock->tm->tm_sec % 10;
+    return;
 }
-return;
-}
-void
-update_hour(void)
+static void draw_number(int n, int x, int y)
 {
- int ihour;
+    int i, sy = y;
 
- cliclock->tm = localtime(&(cliclock->lt));
- cliclock->lt = time(NULL);
+    for(i = 0; i < 30; ++i, ++sy)
+    {
+        if(sy == y + 6)
+        {
+            sy = y;
+            ++x;
+        }
+        wbkgdset(cliclock->framewin, COLOR_PAIR(number[n][i/2]));
+        mvwaddch(cliclock->framewin, x, sy, ' ');
+    }
+    wrefresh(cliclock->framewin);
 
- ihour = cliclock->tm->tm_hour;
-
- /* Set hour */
- cliclock->date.hour[0] = ihour / 10;
- cliclock->date.hour[1] = ihour % 10;
-
- /* Set minutes */
- cliclock->date.minute[0] = cliclock->tm->tm_min / 10;
- cliclock->date.minute[1] = cliclock->tm->tm_min % 10;
-
- /* Set seconds */
- cliclock->date.second[0] = cliclock->tm->tm_sec / 10;
- cliclock->date.second[1] = cliclock->tm->tm_sec % 10;
-
- return;
+    return;
 }
-void
-draw_number(int n, int x, int y)
-{
- int i, sy = y;
-
- for(i = 0; i < 30; ++i, ++sy)
- {
-     if(sy == y + 6)
-     {
-	 sy = y;
-	 ++x;
-     }
- wbkgdset(cliclock->framewin, COLOR_PAIR(number[n][i/2]));
- mvwaddch(cliclock->framewin, x, sy, ' ');
- }
- wrefresh(cliclock->framewin);
-
- return;
-}
-void
+    void
 draw_clock(void)
 {
- /* Draw hour numbers */
- draw_number(cliclock->date.hour[0], 1, 1);
- draw_number(cliclock->date.hour[1], 1, 8);
-
- /* 2 dot for number separation */
- wbkgdset(cliclock->framewin, COLOR_PAIR(1));
- mvwaddstr(cliclock->framewin, 2, 16, "  ");
- mvwaddstr(cliclock->framewin, 4, 16, "  ");
-
- /* Draw minute numbers */
- draw_number(cliclock->date.minute[0], 1, 20);
- draw_number(cliclock->date.minute[1], 1, 27);
-
- /* 2 dot for number separation */
- wbkgdset(cliclock->framewin, COLOR_PAIR(1));
- mvwaddstr(cliclock->framewin, 2, 35, "  ");
- mvwaddstr(cliclock->framewin, 4, 35, "  ");
-
- /* Draw second numbers */
- draw_number(cliclock->date.second[0], 1, 39);
- draw_number(cliclock->date.second[1], 1, 46);
+    /* Draw hour numbers */
+    draw_number(cliclock->date.hour[0], 1, 1);
+    draw_number(cliclock->date.hour[1], 1, 8);
+    /* 2 dot for number separation */
+    wbkgdset(cliclock->framewin, COLOR_PAIR(1));
+    mvwaddstr(cliclock->framewin, 2, 16, "  ");
+    mvwaddstr(cliclock->framewin, 4, 16, "  ");
+    /* Draw minute numbers */
+    draw_number(cliclock->date.minute[0], 1, 20);
+    draw_number(cliclock->date.minute[1], 1, 27);
+    /* 2 dot for number separation */
+    wbkgdset(cliclock->framewin, COLOR_PAIR(1));
+    mvwaddstr(cliclock->framewin, 2, 35, "  ");
+    mvwaddstr(cliclock->framewin, 4, 35, "  ");
+    /* Draw second numbers */
+    draw_number(cliclock->date.second[0], 1, 39);
+    draw_number(cliclock->date.second[1], 1, 46);
 }
-void
-clock_move(int x, int y)
+static void clock_move(int x, int y)
 {
- mvwin(cliclock->framewin, (cliclock->geo.x = x), (cliclock->geo.y = y));
- return;
+    mvwin(cliclock->framewin, (cliclock->geo.x = x), (cliclock->geo.y = y));
+    return;
 }
-void 
-key_event(void)
+static void key_event(void)
 {
-int c;
-struct timespec length = { 0, cliclock->option.delay };
-switch(c = wgetch(stdscr))
-{
-   case 'q':
-	cliclock->running = False;
-	break;
-   default:
-	nanosleep(&length, NULL);
-	break;
+    int c;
+    struct timespec length = { 0, cliclock->option.delay };
+    switch(c = wgetch(stdscr))
+    {
+        case 'q':
+            cliclock->running = false;
+            break;
+        default:
+            nanosleep(&length, NULL);
+            break;
+    }
+    return;
 }
-return;
-}
-int
-main(int argc, char **argv)
+int main(int argc, char **argv)
 {
- // Parses otpions
- int color = DEFAULT_FG_COLOR;
- int c;
- int option_index = 0;
- static struct option long_options[] =
- {
-     {"help", no_argument, 0, 'h'},
-     {"color", required_argument, 0, 'c'}
- };
-
- while ((c = getopt_long (argc, argv, "hc:", long_options, &option_index)) != -1)
- {
-     switch (c)
-     {
-         case 'c':
-         	color = atoi(optarg);
-                if (color < 0 || color > 7)
-                {
-                    printf("Invalid color number: %d\n\n", color);
-                    printf("Valid colors are black (0), red (1),  green (2), yellow (3),\n");
-                    printf("                 blue (4), magenta (5), cyan (6), white (7).\n");
-                    return 1;
-                }
-		break;
-
-         case 'h':
-                usage();
-                return 0;
-     }
- }
-
- // Initializes clock
- cliclock = malloc(sizeof(cliclock_t));
- cliclock->option.color = color;
- cliclock->option.delay = 40000000;
- init();
- while(cliclock->running)
- {
-     update_hour();
-     draw_clock();
-     key_event();
- }
- free(cliclock);
- endwin();
- return 0;
-}
-
-void
-usage (void)
-{
- printf(" Usage: cli-clock [-c color]\n");
- printf("  -c [color]: Use this color for clock (default: %s)\n", DEFAULT_FG_COLOR_NAME);
+    int color = DEFAULT_FG_COLOR;
+    // Initializes clock
+    cliclock = malloc(sizeof(cliclock_t));
+    cliclock->option.color = color;
+    cliclock->option.delay = 40000000;
+    init();
+    while(cliclock->running)
+    {
+        update_hour();
+        draw_clock();
+        key_event();
+    }
+    free(cliclock);
+    endwin();
+    return 0;
 }
